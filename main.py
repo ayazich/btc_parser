0dbfa7c022228676352ce1396bef8dc48942b642
homyak <ayaz.zaripov2004@gmail.com>
2021-09-25 22:36:59 +0300

    add main

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from csgo_manager import CSGOMarket
 
 api_key = ''
 
-connect = CSGOMarket(api_key)
 
-weapon = connect.get_weapon_price(weapon_name='AK-47 | Redline (Field-Tested)')
-
-print(weapon)
+if __name__ == '__main__':
+    connect = CSGOMarket(api_key)
+    weapon = connect.get_weapon_price(weapon_name='AK-47 | Redline (Field-Tested)')
+    print(weapon)
